5e4fbab4443ede79a57a8b167604046e9f509a9a
KIT <nikita002005@gmail.com>
2024-04-12 16:56:27 +0300

    dp c-4

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -17,7 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls.static import static # Это позволит импортировать функци. для получения url адресов
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')) #  не знаю но мы добавили в общие urls для проекта urls приложения store
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
